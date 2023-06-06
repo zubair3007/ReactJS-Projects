327c02fb5ff1f47ec9cda920d7f0dece42cacf2f
Zubair Khan <kzubair129@gmail.com>
2023-06-06 16:02:09 +0530

    Bithday |Reminder project completed

--- 01-birthday-remainder/src/App.jsx
+++ 01-birthday-remainder/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState} from 'react'
+import Data from './Data'
+import List from './List'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
+const [people, setPeople]= useState(Data);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+ <section className="container"> 
+
+ <h3>{people.length} Birthdays Today</h3>
+ <List people={people}/>
+ <button onClick={() => setPeople([])} >Clear All</button>
+
+
+ </section>
+ </main>
   )
 }
 
 export default App
+
